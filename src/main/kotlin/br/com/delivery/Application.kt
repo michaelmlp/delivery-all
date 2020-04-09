2bccafbfab9f834bd71eb61fb6dfadaf9325f38f
Michael de Lelis Pinto <michael.lelis@HOTBHZA002909.hotmart.local>
2020-04-08 22:21:26 -0300

    base project ok

--- src/main/kotlin/br/com/delivery/Application.kt
+++ src/main/kotlin/br/com/delivery/Application.kt
@@ -3,12 +3,18 @@ package br.com.delivery
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @Configuration
 @Order(0)
@@ -21,34 +27,33 @@ open class Application : WebSecurityConfigurerAdapter() {
                 .requestMatchers()
                 .antMatchers(HttpMethod.OPTIONS)
                 .antMatchers("/management/**")
-                .antMatchers("/users")
                 .and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers("/management/health").permitAll()
                 .antMatchers("/management/info").permitAll()
-                .antMatchers("/users").permitAll()
                 .antMatchers("/management/**").hasRole("ACTUATOR").and().httpBasic()
 
         http.csrf().disable()
     }
 
-//    @Bean
-//    fun corsFilter(): FilterRegistrationBean<*> {
-//        val source = UrlBasedCorsConfigurationSource()
-//        val config = CorsConfiguration()
-//        source.registerCorsConfiguration("/**", config)
-//        config.allowCredentials = true
-//
-//        // TODO limit these values
-//        config.addAllowedOrigin("*")
-//        config.addAllowedHeader("*")
-//        config.addAllowedMethod("*")
-//
-//        val bean = FilterRegistrationBean(CorsFilter(source))
-//        bean.order = Ordered.HIGHEST_PRECEDENCE
-//        return bean
-//    }
+    @Bean
+    open fun corsFilter(): FilterRegistrationBean<*> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        source.registerCorsConfiguration("/**", config)
+        config.allowCredentials = true
+
+        // TODO limit these values
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
+    }
+
 
     companion object {
         @JvmStatic
--- src/main/kotlin/br/com/delivery/controller/UserController.kt
+++ src/main/kotlin/br/com/delivery/controller/UserController.kt
@@ -1,9 +1,11 @@
 package br.com.delivery.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/users")
+@RestController
+@RequestMapping("/rest/v1/users")
 class UserController {
 
     @GetMapping
